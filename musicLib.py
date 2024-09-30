6a41210bd4707d15fba2999d84e9ff8d0b2b2bb0
Eish-Agr <149433347+Eish-Agr@users.noreply.github.com>
2024-09-30 23:27:29 +0530

    Add files via upload

---
+++ musicLib.py
@@ -0,0 +1,4 @@
+music = {
+    "popstar":"https://www.youtube.com/watch?v=3CxtK7-XtE0&ab_channel=DJKhaledVEVO",
+    "closer":"https://www.youtube.com/watch?v=PT2_F-1esPk&ab_channel=ChainsmokersVEVO"
+}
